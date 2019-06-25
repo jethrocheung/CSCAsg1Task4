0ce210ef64a9ac4e244844a15195c18042d853e6
JETHROCHEUNG.17 <JETHROCHEUNG.17@ichat.sp.edu.sg>
2019-06-25 16:15:40 +0800

    Add project files.

---
+++ CSCAsg1Task4/Controllers/TalentsController.cs
@@ -0,0 +1,75 @@
+﻿using CSCAsg1Task4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace CSCAsg1Task4.Controllers
+{
+    public class TalentsController : ApiController
+    {
+        static readonly ITalentRepository repository = new TalentRepository();
+        [HttpGet]
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        [Route("api/talents")]
+        public IEnumerable<Talent> GetAllTalents()
+        {
+            return repository.GetAll();
+        }
+
+        [HttpGet]
+        [Route("api/talents/{id:int:min(2)}", Name = "getTalentById")]
+        public Talent GetTalent(int id)
+        {
+            Talent item = repository.Get(id);
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return item;
+        }
+
+        //PUT, POST and DELETE
+        [HttpPost]
+        [Route("api/talents")]
+        public HttpResponseMessage PostTalent(Talent item)
+        {
+            if (ModelState.IsValid)
+            {
+                item = repository.Add(item);
+                var response = Request.CreateResponse<Talent>(HttpStatusCode.Created, item);
+
+                // Generate a link to the new talent and set the Location header in the response.
+
+                string uri = Url.Link("getTalentById", new { id = item.Id });
+                response.Headers.Location = new Uri(uri);
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        [HttpPut]
+        [Route("api/talents/{id:int}")]
+        public void PutTalent(int id, Talent talent)
+        {
+            talent.Id = id;
+            if (!repository.Update(talent))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/talents/{id:int}")]
+        public void DeleteTalent(int id)
+        {
+            repository.Remove(id);
+        }
+    }
+}
